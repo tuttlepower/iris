63b83fc434f3b954c93ee4ab9f6c9b3290b7c6ae
tuttlepower <tatuttle2015@gmail.com>
2021-08-01 19:14:10 -0400

    data cuts

--- iris.py
+++ iris.py
@@ -14,7 +14,10 @@ url_list = {
     'MIT Data': 'https://news.mit.edu/rss/topic/data-management-and-statistics',
     'EconTalk': 'https://feeds.simplecast.com/wgl4xEgL',
     'NPR': 'https://feeds.npr.org/1001/rss.xml',
-    'NBER':'https://back.nber.org/rss/new.xml'
+    'NBER':'https://back.nber.org/rss/new.xml',
+    'BEA':'https://apps.bea.gov/rss/rss.xml',
+    'Arxiv_econ':'http://export.arxiv.org/rss/econ',
+    'JMLR':"https://jmlr.org/jmlr.xml",
 }
 icon_list = {
     url_list['FiveThirtyEight']: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/FiveThirtyEight_Logo.svg/1280px-FiveThirtyEight_Logo.svg.png',
@@ -23,21 +26,25 @@ icon_list = {
     url_list['MIT Data']: 'https://dsl.mit.edu/sites/default/files/Final%20Zoomed.png',
     url_list['EconTalk']: 'http://files.libertyfund.org/econtalk/EconTalkCDcover1400y2007.jpg',
     url_list['NPR']: 'https://media.npr.org/chrome_svg/npr-logo.svg',
-    url_list['NBER']:'https://cdn.corporatefinanceinstitute.com/assets/national-bureau-of-economic-research-nber1.jpg'
+    url_list['NBER']:'https://cdn.corporatefinanceinstitute.com/assets/national-bureau-of-economic-research-nber1.jpg',
+    url_list['BEA']:'https://www.commerce.gov/sites/default/files/styles/full_width_image_700x400max/public/media/images/branding/bea-logo-dark-background-citing.png?itok=C_RcEoKT',
+    url_list['Arxiv_econ']:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/ArXiv_web.svg/1200px-ArXiv_web.svg.png',
+    url_list['JMLR']:'https://jmlr.org/img/jmlr.jpg',
 }
 # TODO: Fix Looping to improve speed, cut dataset size, no need to go through all of Econtalk
+# Limit the data BEFORE looping through, don't loop and cut, cut it off before looping
 def getArticles(url):
     r = requests.get(url)
     root = ET.fromstring(r.text)
     articles = []
     root = root[0]
     old = False
-    while not old:
+    while not old and articles.__len__() < 10:
         for item in root:
             title = ''
             link = ''
             description = ''
-            date = ''
+            date = datetime.now().date()
             for article in item:
                 if(article.tag == 'title'):
                     title = article.text
@@ -46,17 +53,17 @@ def getArticles(url):
                 if (article.tag == 'description'):
                     description = article.text
                 if (article.tag == 'pubDate'):
-                    date = article.text
+                    temp_date = article.text
                     try:
                         date = datetime.strptime(
-                            date, '%a, %d %b %Y %H:%M:%S %z').date()
+                            temp_date, '%a, %d %b %Y %H:%M:%S %z').date()
                     except:
                         date = datetime.now().date()
 
             if(title != '' and link != '' and description != '' and date > datetime.now().date() - timedelta(days=7)):
                 p = Paper(title, description, link, date, logo=icon_list[url])
                 articles.append(p)
-        return articles
+        return articles[1:10]
 
 
 def getPapersFromAllUrls():
