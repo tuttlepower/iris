8a9397ba68c540843f8274666550e58d433d53b7
Tuttlepower <tatuttle2015@gmail.com>
2023-03-12 15:16:59 -0400

    api/app.py refactor

--- Paper.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-class Paper:
-    def __init__(self, title, description, link, date=datetime.now().date(), logo='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'):
-        self.title = title
-        self.description = description
-        self.link = link
-        self.date = date
-        self.logo = logo
