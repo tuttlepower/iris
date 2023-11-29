9f425d6c02447d2d05c0ef420248c593b900e87a
Tuttlepower <tatuttle2015@gmail.com>
2023-11-28 20:37:31 -0500

    adding json

--- api/index.py
+++ api/index.py
@@ -1,14 +1,21 @@
 from flask import Flask
 import os
+from flask_json import FlaskJSON
 
 app = Flask(__name__)
+FlaskJSON(app)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
 @app.route('/healthcheck')
 def healthcheck():
     return "Healthcheck Endpoint"
+
+@app.route('/get_data')
+def get_data():
+    return json_response(data={"key": "value"})
 
 
 # @app.route('/')
