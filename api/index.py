c3c7c18c86ec254cf071f5fecbf61e83d235644b
Tuttlepower <tatuttle2015@gmail.com>
2023-11-28 20:33:02 -0500

    base case

--- api/index.py
+++ api/index.py
@@ -1,25 +1,21 @@
-from dataclasses import dataclass
-from datetime import datetime
-
 from flask import Flask
-from flask_cors import CORS, cross_origin
-from flask_json import FlaskJSON, as_json, json_response
-
-
+import os
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def home():
+    return 'Hello, World!'
 @app.route('/healthcheck')
 def healthcheck():
     return "Healthcheck Endpoint"
 
 
-@app.route('/')
-def data():
-    return json_response(data="Data")
+# @app.route('/')
+# def data():
+#     return json_response(data="Data")
 
 
-@app.route('/get_time')
-def get_time():
-    return json_response(time=datetime.utcnow())
+# @app.route('/get_time')
+# def get_time():
+#     return json_response(time=datetime.utcnow())
