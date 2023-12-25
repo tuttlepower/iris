4c128e86d811d5d2a0764ea3f1ec78c5ce2c2b40
Tuttlepower <tatuttle2015@gmail.com>
2023-12-24 21:01:35 -0500

    render redirect etc

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect,render_template
+from flask import Flask, redirect,render_template,session, url_for, request,jsonify,send_from_directory
 from flask import jsonify
 import datetime
 import requests
